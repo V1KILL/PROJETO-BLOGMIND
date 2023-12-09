833b331af2d660307a62a7397ba479a99638f37f
Victor Amaral Mendonça <canalv1kill@gmail.com>
2023-12-08 21:10:59 -0300

    MarkDown Updated

--- app_blogmind/templatetags/blog_extras.py
+++ app_blogmind/templatetags/blog_extras.py
@@ -7,4 +7,4 @@ register = template.Library()
 @register.filter
 @stringfilter
 def convert_markdown(value):
-    return markdown.markdown(value, extensions=['markdown.extensions.fenced_code'])
+    return markdown.markdown(value, extensions=['markdown.extensions.fenced_code', "codehilite"])
