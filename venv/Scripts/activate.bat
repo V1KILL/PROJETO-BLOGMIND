914ecfd3b5c77536d44ea70170fc93283ba5b8a5
Victor Amaral Mendonça <canalv1kill@gmail.com>
2024-07-14 00:47:48 -0300

    Updated

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Victor\Desktop\PROJETOS OFICIAIS\PROJETO - BLOG\PROJETO-BLOG\venv
+set VIRTUAL_ENV=C:\Users\Victor\Desktop\PROJETOS OFICIAIS\PROJETO-BLOGMIND\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
